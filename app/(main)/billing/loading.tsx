a3d7a8a5c06d89cf974ac5a32cad7f52a7f21e11
m7bdi3 <m7mdbadi3@gmail.com>
2025-02-06 23:19:46 +0300

    Add loading components for billing and success pages with typewriter animation

---
+++ app/(main)/billing/loading.tsx
@@ -0,0 +1,13 @@
+export default function loading() {
+  return (
+    <div className="h-screen w-full flex items-center justify-center">
+      <div className="typewriter">
+        <div className="slide">
+          <i></i>
+        </div>
+        <div className="paper"></div>
+        <div className="keyboard"></div>
+      </div>
+    </div>
+  );
+}
